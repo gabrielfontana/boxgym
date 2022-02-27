b901831ff28c00d3edcfd5db66afd045ea46dfed
Gabriel Fontana <gabrielf.junqueira74@gmail.com>
2022-02-27 17:25:56 -0300

    Atualização do método start() da classe Main

--- BoxGym/src/boxgym/Main.java
+++ BoxGym/src/boxgym/Main.java
@@ -10,13 +10,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource(LOGIN_VIEW)); //Carrega FXML          
         Scene scene = new Scene(root); //Coloca o FXML em uma cena
-        primaryStage.setResizable(false); //Não permitir a maximização da janela
-        primaryStage.setTitle(LOGIN_TITLE); //Título da janela
-        primaryStage.setScene(scene); //Coloca a cena em uma janela
-        primaryStage.show(); //Abre a janela
+        stage.setResizable(false); //Não permitir a maximização da janela
+        stage.setTitle(LOGIN_TITLE); //Título da janela
+        stage.setScene(scene); //Coloca a cena em uma janela
+        stage.show(); //Abre a janela
     }
 
     public static void main(String[] args) {
