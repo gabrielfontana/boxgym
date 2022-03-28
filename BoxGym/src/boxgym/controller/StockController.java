df6da5551a81de47e2985d4556b91cf9d4eb1638
Gabriel Fontana <gabrielf.junqueira74@gmail.com>
2022-03-28 08:15:14 -0300

    Mudança no layout dos textfields

---
+++ BoxGym/src/boxgym/controller/StockController.java
@@ -0,0 +1,23 @@
+package boxgym.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class StockController implements Initializable {
+
+    @FXML
+    private Button addButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+
+    @FXML
+    void add(ActionEvent event) {
+        
+    }
+}
